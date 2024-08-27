7e1e9c7fc6a33c97674eed57fed85b247cb52e0a
marcio <marcio.osshiro@ifms.edu.br>
2024-08-26 20:16:54 -0400

    27 de agosto

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -118,6 +118,12 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/categoria">Categoria</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/venda/listar">Vendas1</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/venda/listar2">Vendas2</a>
+        </li>
       </ul>
     </div>
   </div>
